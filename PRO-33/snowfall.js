e5c602f393d841b0b9f445d91e88a048d674716f
Husain3090 <74500292+Husain3090@users.noreply.github.com>
2021-06-22 11:39:00 +0530

    Add files via upload

---
+++ PRO-33/snowfall.js
@@ -0,0 +1,12 @@
+class Snowfall{
+
+    constructor(){
+        var options = {isStatic:false}
+        this.snowfall=Bodies.rectangle(random(1,800),10,10,10,options)
+        World.add(world,this.snowfall)
+    }
+    display(){
+        imageMode(CENTER)
+        image(snowImage,this.snowfall.position.x,this.snowfall.position.y,30,30)
+    }
+}
